ef76655ae235a72e8e363592efcf03f0b597e17e
WAVIBOI-on-YouTube <111872195+WAVIBOI-on-YouTube@users.noreply.github.com>
2022-08-28 15:08:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
 import time
 import threading
-from pynput.mouse import Controller, Button
+from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 
-print("Hello! Press T on your keyboard to the Autoclicker on or off.")
+print("Hello! Press ` on your keyboard to the Autoclicker on or off. You can close this app by clicking K.")
 print("Note: Only use in games which allow Autoclicker or singleplayer games.")
 print("Made by W A V I B O I.")
 
-TOGGLE_KEY = KeyCode(char="t")
+delay = 0.00000000000001
+button = Button.left
+start_stop_key = KeyCode(char='`')
+exit_key = KeyCode(char='k')
 
-clicking = False
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_run = True
+
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_run = False
+
+    def run(self):
+        while self.program_run:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(1)
+
+
 mouse = Controller()
+thread = ClickMouse(delay, button)
+thread.start()
 
 
-def clicker():
-    while True:
-        if clicking:
-            mouse.click(Button.left, 1)
-        time.sleep(0.000001)
+def on_press(key):
+    if key == start_stop_key:
+        if thread.running:
+            thread.stop_clicking()
+        else:
+            thread.start_clicking()
+    elif key == exit_key:
+        thread.exit()
+        listener.stop()
 
 
-def toggle_event(key):
-    if key == TOGGLE_KEY:
-        global clicking
-        clicking = not clicking
-
-
-click_thread = threading.Thread(target=clicker)
-click_thread.start()
-
-with Listener(on_press=toggle_event) as listener:
+with Listener(on_press=on_press) as listener:
     listener.join()
